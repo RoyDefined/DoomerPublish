e34808197f08206fc8ca148757db031a31b928d7
RoyDefined <145583009+RoyDefined@users.noreply.github.com>
2023-11-14 16:23:30 +0100

    Improved enum parsing, see description
    - BCC enums are not parsed if the value contains 0-9 or =.
    - Private enums are parsed regardless the fact that they are unusable outside of the implementing library.
    - Enums that contain a bitshift are not properly parsed.
    - Non-integer enums are not properly parsed.

--- src/DoomerPublish/DoomerPublish.lib/Tools/AcsParseService/Parse/Default/DefaultEnumParser.cs
+++ src/DoomerPublish/DoomerPublish.lib/Tools/AcsParseService/Parse/Default/DefaultEnumParser.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.Extensions.Logging;
 using System.Globalization;
+using System.Linq;
 using System.Text.RegularExpressions;
 
 namespace DoomerPublish.Tools.Acs;
@@ -15,7 +16,7 @@ internal sealed class DefaultEnumParser : IAcsParser
 	/// <summary>
 	/// Regex to find enum definitions, which will return the inner enum content.
 	/// </summary>
-	private readonly Regex _enumContentRegex = new(@"enum .* \s*{(?<enumContent>([\sa-z\/,.`\(\)]*))};", RegexOptions.IgnoreCase);
+	private readonly Regex _enumContentRegex = new(@"(?<isPrivate>(private ))?enum \w*\s*(:\s*(?<enumType>(fixed|int|str)))?\s*{(?<enumContent>([\sa-z0-9_\-\/\""\<\>,.'`\(\)\=]*))};", RegexOptions.IgnoreCase);
 
 	public DefaultEnumParser(
 		ILogger<DefaultEnumParser> logger)
@@ -27,33 +28,28 @@ internal sealed class DefaultEnumParser : IAcsParser
 	public Task ParseAsync(AcsFile acsFile, CancellationToken _)
 	{
 		var enumMatchCollection = this._enumContentRegex.Matches(acsFile.Content);
+		acsFile.EnumLibdefines = new();
 
-		var filteredLines = enumMatchCollection.Select(x =>
+		foreach(var match in enumMatchCollection.ToList())
 		{
-			var enumContent = x.Groups.GetValueOrDefault("enumContent")?.Value ??
-				throw new InvalidOperationException($"Expected enum content for {x.Name}");
+			var isPrivate = match.Groups.GetValueOrDefault("isPrivate")?.Success ?? false;
 
-			// Get individual lines, removing whitespace and comments.
-			return enumContent
-				.Split(new string[] { "\r\n", "\r", "\n" }, StringSplitOptions.None)
-				.Where(y =>
-				{
-					var trimmedString = y.Trim();
-					return !trimmedString.StartsWith("//", StringComparison.OrdinalIgnoreCase) && !string.IsNullOrEmpty(trimmedString);
-				})
-				.Select(y =>
-				{
-					if (y.EndsWith(",", StringComparison.OrdinalIgnoreCase))
-					{
-						return y[..^1].Trim();
-					}
+			// Skip if private
+			if (isPrivate) {
+				continue;
+			}
 
-					return y.Trim();
-				}).ToList();
-		}).ToList();
+			// The enum is of type "integer" if nothing is specified.
+			var enumType = match.Groups.GetValueOrDefault("enumType")?.Value;
+			var isIntegerType = string.IsNullOrEmpty(enumType) || enumType.ToUpperInvariant() == "INT";
 
-		acsFile.EnumLibdefines = this.EnumerateFilteredEnumLines(filteredLines)
-			.ToList();
+			var enumContent = match.Groups.GetValueOrDefault("enumContent")?.Value ??
+				throw new InvalidOperationException($"Expected enum content for {match.Name}");
+
+			var filteredEnumLines = this.FilterEnumContent(enumContent);
+			var enumLibDefines = this.EnumerateFilteredEnumLines(filteredEnumLines, isIntegerType);
+			acsFile.EnumLibdefines.AddRange(enumLibDefines);
+		}
 
 		var count = acsFile.EnumLibdefines.Count;
 		if (count > 0)
@@ -63,29 +59,85 @@ internal sealed class DefaultEnumParser : IAcsParser
 		return Task.CompletedTask;
 	}
 
-	private IEnumerable<AcsLibdefine> EnumerateFilteredEnumLines(List<List<string>> lines)
+	private IEnumerable<string> FilterEnumContent(string content)
 	{
-		// If the enum has an equality after it, then we must continue with that number.
-		foreach (var filteredCollection in lines)
+		var lines = content.Split(new string[] { "\r\n", "\r", "\n" }, StringSplitOptions.None);
+		var linesNoComment = lines.Where(y =>
 		{
-			var value = 0;
-			foreach (var line in filteredCollection)
+			var trimmedString = y.Trim();
+			return !trimmedString.StartsWith("//", StringComparison.OrdinalIgnoreCase) && !string.IsNullOrEmpty(trimmedString);
+		});
+		var linesNoComma = linesNoComment.Select(y =>
+		{
+			if (y.EndsWith(",", StringComparison.OrdinalIgnoreCase))
 			{
-				var parts = line.Split("=");
-				if (parts.Length is not 1 or 2)
-				{
-					throw new InvalidOperationException("Expected one or two parts for enum definition");
-				}
+				return y[..^1].Trim();
+			}
 
-				if (parts.Length == 2)
+			return y.Trim();
+		});
+
+		return linesNoComma;
+	}
+
+	private IEnumerable<AcsLibdefine> EnumerateFilteredEnumLines(IEnumerable<string> lines, bool isIntegerType)
+	{
+		// If parsing an enum that consists of integers, we increment the value if none is given.
+		var valueIncrement = 0;
+		foreach (var line in lines)
+		{
+			var parts = line.Split("=");
+			if (parts.Length is not 1 and not 2)
+			{
+				throw new InvalidOperationException($"Failed for \"{line}\". Expected one or two parts for enum definition.");
+			}
+
+			var key = parts.ElementAt(0).Trim();
+			var value = parts.ElementAtOrDefault(1)?.Trim();
+
+			if (!isIntegerType)
+			{
+				// Any other type has an explicit value we use.
+				if (value == null)
 				{
-					value = int.Parse(parts.Last(), CultureInfo.InvariantCulture);
+					throw new InvalidOperationException($"Failed for \"{line}\". Non-integer enum should have a key and value.");
 				}
 
 				yield return new()
 				{
-					Key = parts.First(),
-					Value = value++.ToString(CultureInfo.InvariantCulture),
+					Key = key,
+					Value = value,
+				};
+
+				continue;
+			}
+
+			if (value == null)
+			{
+				yield return new()
+				{
+					Key = key,
+					Value = valueIncrement++.ToString(CultureInfo.InvariantCulture),
+				};
+
+				continue;
+			}
+
+			// Attempt to parse the enum. This can fail if the integer has a bitshift.
+			if (int.TryParse(value, CultureInfo.InvariantCulture, out valueIncrement))
+			{
+				yield return new()
+				{
+					Key = parts[0],
+					Value = valueIncrement.ToString(CultureInfo.InvariantCulture),
+				};
+			}
+			else
+			{
+				yield return new()
+				{
+					Key = key,
+					Value = value,
 				};
 			}
 		}
